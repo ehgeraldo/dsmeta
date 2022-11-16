8a06853ccf80b1c6de79ba05c6761536c4093f87
ehgeraldo <ehgeraldo@gmail.com>
2022-11-16 10:13:01 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./assets/components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
